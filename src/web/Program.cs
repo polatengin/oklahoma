6d112a1d792c18cd9200486ab326074f5e208450
polatengin <enpolat@microsoft.com>
2024-08-27 13:51:31 -0700

    adding table service client to DI

--- src/web/Program.cs
+++ src/web/Program.cs
@@ -1,6 +1,13 @@
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
+builder.Services.AddSingleton<TableServiceClient>(sp =>
+{
+  var configuration = sp.GetRequiredService<IConfiguration>();
+  var connectionString = configuration.GetConnectionString("StorageAccount");
+  return new TableServiceClient(connectionString);
+});
+
 app.MapGet("/", () => "Hello World!");
 
 app.Run();
