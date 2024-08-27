2b9c095f37e2c24e3a16eac5392a6210de18dd15
polatengin <enpolat@microsoft.com>
2024-08-27 11:52:07 -0700

    creating customer class

--- src/cli/Program.cs
+++ src/cli/Program.cs
@@ -1,4 +1,4 @@
-Console.WriteLine("Azure Storage Table - Getting Started sample\n");
+﻿Console.WriteLine("Azure Storage Table - Getting Started sample\n");
 
 var accountName = Environment.GetEnvironmentVariable("AZURE_STORAGE_ACCOUNT_NAME");
 var storageAccountKey = Environment.GetEnvironmentVariable("AZURE_STORAGE_ACCOUNT_KEY");
@@ -22,6 +22,7 @@ Console.WriteLine("Customer saved.");
 var retrieved = await tableClient.GetEntityAsync<Customer>("Smith", "John");
 var existingCustomer = retrieved.Value;
 Console.WriteLine($"Retrieved customer: {existingCustomer.FirstName} {existingCustomer.LastName}, Age: {existingCustomer.Age}");
+
 existingCustomer.Age = 43;
 await tableClient.UpdateEntityAsync(existingCustomer, existingCustomer.ETag);
 Console.WriteLine("Customer updated.");
@@ -32,7 +33,29 @@ foreach (var c in customers)
 {
   Console.WriteLine($"- {c.FirstName}: {c.LastName}, Age: {c.Age}");
 }
+
 Console.WriteLine($"The query returned {customers.Count()} entities.");
 
 await tableClient.DeleteEntityAsync(existingCustomer.PartitionKey, existingCustomer.RowKey, existingCustomer.ETag);
 Console.WriteLine("Customer deleted.");
+
+public class Customer : ITableEntity
+{
+  public string PartitionKey { get; set; }
+  public string RowKey { get; set; }
+  public DateTimeOffset? Timestamp { get; set; }
+  public ETag ETag { get; set; }
+
+  public string FirstName { get; set; }
+  public string LastName { get; set; }
+
+  public byte Age { get; set; } = 0;
+
+  public Customer(string lastName, string firstName)
+  {
+    PartitionKey = lastName;
+    RowKey = firstName;
+    FirstName = firstName;
+    LastName = lastName;
+  }
+}
