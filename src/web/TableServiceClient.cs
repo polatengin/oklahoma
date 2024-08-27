74d06c743472e55b6cd98a724bfd1e77b5dcb16f
polatengin <enpolat@microsoft.com>
2024-08-27 13:51:09 -0700

    creating initial generic table service client

---
+++ src/web/TableServiceClient.cs
@@ -0,0 +1,38 @@
+public class TableService<T> where T : class, ITableEntity
+{
+  private readonly TableClient _tableClient;
+
+  public TableService(TableServiceClient tableServiceClient, string tableName)
+  {
+    _tableClient = tableServiceClient.GetTableClient(tableName);
+    _tableClient.CreateIfNotExists();
+  }
+
+  public async Task AddAsync(T entity)
+  {
+    await _tableClient.AddEntityAsync(entity);
+  }
+
+  public async Task<T?> GetAsync(string partitionKey, string rowKey)
+  {
+    try
+    {
+      var response = await _tableClient.GetEntityIfExistsAsync<T>(partitionKey, rowKey);
+      return response.Value;
+    }
+    catch (RequestFailedException)
+    {
+      return null;
+    }
+  }
+
+  public async Task UpdateAsync(T entity)
+  {
+    await _tableClient.UpdateEntityAsync(entity, ETag.All, TableUpdateMode.Replace);
+  }
+
+  public async Task DeleteAsync(string partitionKey, string rowKey)
+  {
+    await _tableClient.DeleteEntityAsync(partitionKey, rowKey);
+  }
+}
