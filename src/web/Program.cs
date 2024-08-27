3acc1f29fce1d7197433772d6a5b9c7ac1cc82f7
polatengin <enpolat@microsoft.com>
2024-08-27 15:12:52 -0700

    updating DI container

--- src/web/Program.cs
+++ src/web/Program.cs
@@ -1,18 +1,21 @@
 var builder = WebApplication.CreateBuilder(args);
 
+builder.Services.AddHttpContextAccessor();
 builder.Services.AddScoped<HttpContextAccessor, HttpContextAccessor>();
 
 builder.Services.AddSingleton<TableServiceClient>(sp =>
 {
-  var configuration = sp.GetRequiredService<IConfiguration>();
-  var connectionString = configuration.GetConnectionString("StorageAccount");
+  var accountName = Environment.GetEnvironmentVariable("AZURE_STORAGE_ACCOUNT_NAME");
+  var storageAccountKey = Environment.GetEnvironmentVariable("AZURE_STORAGE_ACCOUNT_KEY");
+  var connectionString = $"DefaultEndpointsProtocol=https;AccountName={accountName};AccountKey={storageAccountKey};EndpointSuffix=core.windows.net";
+
   return new TableServiceClient(connectionString);
 });
 
 builder.Services.AddSingleton<TableService<Customer>>(sp =>
 {
   var tableServiceClient = sp.GetRequiredService<TableServiceClient>();
-  return new TableService<Customer>(tableServiceClient, "customers");
+  return new TableService<Customer>(tableServiceClient, Environment.GetEnvironmentVariable("AZURE_STORAGE_ACCOUNT_TABLE_NAME") ?? "customers");
 });
 
 builder.Services.AddScoped<CustomerCreateHandler>();
